76905499b9bab370d4b4e6bf1f877ad00d119c21
Sergey <sergey.m.683@gmail.com>
2019-10-08 12:35:04 +0300

    Добавляет рендер комментарий

--- js/main.js
+++ js/main.js
@@ -2,7 +2,12 @@
 
 var COMMENTS = ['Всё отлично!', 'В целом всё неплохо. Но не всё.Когда вы делаете фотографию, хорошо бы убирать палец из кадра.', 'Вконце концов это просто непрофессионально.', 'Моя бабушка случайно чихнула с фотоаппаратом в руках и у неё получилась фотография лучше.', 'Я поскользнулся на банановой кожуре и уронил фотоаппарат на кота и у меня получилась фотография лучше.', 'Лица у людей на фотке перекошены, как будто их избивают. Как можно было поймать такой неудачный момент?!'];
 var USERS_NAMES = ['Василий', 'Геннадий', 'Алексей', 'Иван'];
+var DESCRIPTIONS_PHOTOS = ['Новый день', 'Игра воображения', 'Песня осеннего пламени'];
+var COMMENTS_COUNT = 5;
 var descriptionUserCount = 25;
+var descriptionListElement = document.querySelector('.pictures');
+var bigpictureListElement = document.querySelector('.big-picture');
+
 var generateRandomNumber = function (userNumber) {
   var randomNumber = Math.floor(Math.random() * userNumber);
   if (randomNumber === 0) {
@@ -10,7 +15,6 @@ var generateRandomNumber = function (userNumber) {
   }
   return randomNumber;
 };
-var descriptionListElement = document.querySelector('.pictures');
 
 
 var randomArrayElement = function (array) {
@@ -21,7 +25,7 @@ var descriptionUser = [];
 for (var i = 0; i < descriptionUserCount; i++) {
   var descriptionUsers = {
     url: 'photos/' + generateRandomNumber(25) + '.jpg',
-    description: 'Описание',
+    description: randomArrayElement(DESCRIPTIONS_PHOTOS),
     likes: generateRandomNumber(600),
     comments: [
       {
@@ -53,3 +57,29 @@ for (var j = 0; j < descriptionUser.length; j++) {
 }
 
 descriptionListElement.appendChild(fragment);
+//======
+bigpictureListElement.querySelector('.big-pictures__img').src = descriptionUser[0].url;
+bigpictureListElement.querySelector('.social__caption').textContent = descriptionUser[0].description;
+bigpictureListElement.querySelector('.likes-count').textContent = descriptionUser[0].likes;
+bigpictureListElement.querySelector('.comments-count').textContent = generateRandomNumber(100);
+//======
+
+var commentsUsersTemplate = document.querySelector('.social__comment');
+var commentsListElement = document.querySelector('.social__comments');
+
+var renderCommentsUser = function (descriptionUserComments) {
+  var commentsUserElement = commentsUsersTemplate.cloneNode(true);
+  commentsUserElement.querySelector('.social__picture').src = descriptionUserComments.comments[0].avatar;
+  commentsUserElement.querySelector('.social__picture').alt = descriptionUserComments.comments[0].name;
+  commentsUserElement.querySelector('.social__text').textContent = descriptionUserComments.comments[0].message;
+  return commentsUserElement;
+};
+
+var fragmentComments = document.createDocumentFragment();
+for (var q = 0; q < COMMENTS_COUNT; q++) {
+  fragmentComments.appendChild(renderCommentsUser(descriptionUser[q]));
+}
+
+commentsListElement.appendChild(fragmentComments);
+document.querySelector('.social__comment-count').classList.add('visually-hidden');
+document.querySelector('.comments-loader').classList.add('visually-hidden');
